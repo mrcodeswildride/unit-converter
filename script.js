3a8f2cecb934811b9e578105d46267cb02149b1c
David Keller <mrcodeswildride@gmail.com>
2020-08-04 17:07:06 +0000

    added two more conversions

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+let inchesButton = document.getElementById(`inchesButton`)
+let inchesParagraph = document.getElementById(`inchesParagraph`)
+let feet = document.getElementById(`feet`)
+
+let secondsButton = document.getElementById(`secondsButton`)
+let secondsParagraph = document.getElementById(`secondsParagraph`)
+let minutes = document.getElementById(`minutes`)
+
 let fahrenheitButton = document.getElementById(`fahrenheitButton`)
 let fahrenheitParagraph = document.getElementById(`fahrenheitParagraph`)
 let celsius = document.getElementById(`celsius`)
 
+inchesButton.addEventListener(`click`, showInches)
+secondsButton.addEventListener(`click`, showSeconds)
 fahrenheitButton.addEventListener(`click`, showFahrenheit)
+
+function showInches() {
+  let feetValue = feet.value.trim()
+
+  if (feetValue == `` || isNaN(feetValue)) {
+    inchesParagraph.innerHTML = `Please type a number.`
+  }
+  else {
+    let inchesValue = feetValue * 12
+    inchesParagraph.innerHTML = `${feetValue} feet is ${inchesValue} inches.`
+  }
+}
+
+function showSeconds() {
+  let minutesValue = minutes.value.trim()
+
+  if (minutesValue == `` || isNaN(minutesValue)) {
+    secondsParagraph.innerHTML = `Please type a number.`
+  }
+  else {
+    let secondsValue = minutesValue * 60
+    secondsParagraph.innerHTML = `${minutesValue} minutes is ${secondsValue} seconds.`
+  }
+}
 
 function showFahrenheit() {
   let celsiusValue = celsius.value.trim()
